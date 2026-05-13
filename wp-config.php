5e81abde6c1876b1cba117183f632c458447966f
Suraj <info@5uraj.nl>
2026-05-13 10:13:27 +0200

    Correct config

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wp_5etup' );
+define( 'DB_NAME', 'wp_imvire' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
